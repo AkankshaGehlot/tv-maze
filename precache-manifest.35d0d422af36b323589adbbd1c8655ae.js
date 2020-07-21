601c1029b021a3f7377b43b75629e618130099ab
Akanksha <akankshagehlot1220@gmail.com>
2020-07-22 03:17:21 +0530

    Updates

---
+++ precache-manifest.35d0d422af36b323589adbbd1c8655ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97eb3979a1b88a15cdb2cda063f9757",
+    "url": "/tv-maze/index.html"
+  },
+  {
+    "revision": "86efc266a1e8095b40e7",
+    "url": "/tv-maze/static/css/main.423d1282.chunk.css"
+  },
+  {
+    "revision": "e83c7bc4f61d6bf552fd",
+    "url": "/tv-maze/static/js/2.384fdbce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tv-maze/static/js/2.384fdbce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86efc266a1e8095b40e7",
+    "url": "/tv-maze/static/js/main.a6114cdf.chunk.js"
+  },
+  {
+    "revision": "5edb686f1231e1084c3e",
+    "url": "/tv-maze/static/js/runtime-main.43857376.js"
+  }
+]);
